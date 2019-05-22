efda96a6165d38edcc464d9a8a53b73f8dc33d4f
kosuke.kuzuoka <kousuke.newlife@gmail.com>
2019-05-22 17:59:24 +0900

    Revised answer

--- LeetCode/74_search_a_2d_matrix.py
+++ LeetCode/74_search_a_2d_matrix.py
@@ -3,13 +3,12 @@ Solution for 74. Search a 2D Matrix
 https://leetcode.com/problems/search-a-2d-matrix/
 """
 
-
 class Solution:
     """
-    Runtime: 40 ms, faster than 81.48% of Python3 online submissions for Search a 2D Matrix.
-    Memory Usage: 13.9 MB, less than 5.71% of Python3 online submissions for Search a 2D Matrix
+    Runtime: 36 ms, faster than 96.37% of Python3 online submissions for Search a 2D Matrix.
+    Memory Usage: 14 MB, less than 33.84% of Python3 online submissions for Search a 2D Matrix.
     """
-    def simple_approach(self, matrix, target):
+    def searchMatrix(self, matrix, target):
         """
         Simple approach
         Args:
@@ -19,118 +18,81 @@ class Solution:
         Returns:
             bool: True if target exists in the matrix, otherwise False
         """
+        # Check if the matrix is empty, if so return False
         if len(matrix) == 0:
             return False
 
         if len(matrix[0]) == 0:
             return False
 
-        # Placeholder for a row
-        target_row = None
+        # Determine which row we should look for
+        row = self.vertical_search(matrix, target)
 
-        # Loop for each row, and pick by checking if target falls into the range
-        for row in matrix:
-            if row[0] <= target <= row[-1]:
-                target_row = row
-                break
+        # Given the row, do the binary search to determine if the value exists or not
+        return self.horizontal_search(matrix[row], target)
 
-        if target_row:
-            # Do the binary search to determine whether the target is there or not
-            return self.binary_search(target_row, target)
-        else:
-            return False
-
-    def binary_search(self, row, target):
+    def vertical_search(self, matrix, target):
         """
-        An iterative binary search method
+        This determines which row we should look for to find the target value
         Args:
-            row: list to look a target from
-            target: integer value to look for
+            matrix: 2D matrix to look a target from
+            target: an integer value to look for
 
         Returns:
-            bool: True if value exists in row, False otherwise
+            bool: True if target exists in the matrix, otherwise False
         """
-        low = 0
-        high = len(row) - 1
+        # Initialize low and high
+        low, high = 0, len(matrix) - 1
 
+        # Keep dividing in half, until the low exceeds the high
         while low <= high:
+
+            # Create a pointer that points the middle value
             mid = (low + high) // 2
 
+            # If the value falls into the range, then we know that row should contain the target
+            if matrix[mid][0] <= target <= matrix[mid][-1]:
+                return mid
+
+            # If the middle row's first is larger than the target, then look for the left half
+            elif target < matrix[mid][0]:
+                high = mid - 1
+
+            # If the middle row's last element is smaller than the target, the look for right half
+            elif target > matrix[mid][-1]:
+                low = mid + 1
+
+        return -1
+
+    def horizontal_search(self, row, target):
+        """
+        This function determines if the target value exists in the row or not
+        Args:
+            row: 1D array where each element is integer
+            target: int value to look from row
+
+        Returns:
+            bool: True if the target value exists in the row, otherwise False will be returned
+        """
+        # Initialize low and high for a binary search
+        low, high = 0, len(row) - 1
+
+        # Keep divining into a half, until the left point exceeds the right pointer
+        while low <= high:
+            # Get the middle pointer
+            mid = (low + high) // 2
+
+            # If the middle value is the one we are looking for, then return True
             if row[mid] == target:
                 return True
-            elif row[mid] > target:
+
+            # If the middle value is larger than the target, then look for the left half
+            elif target < row[mid]:
                 high = mid - 1
+
+            # If the middle value is smaller than the target, then look for the right half
             else:
                 low = mid + 1
 
+        # If the code reaches to this point, then this means we couldn't find the target
         return False
-
-    def optimal_approach(self, matrix, target):
-        """
-        Optimal solution
-        Args:
-            matrix: 2D matrix to look a target from
-            target: an integer value to look for
-
-        Returns:
-            bool: True if target exists in the matrix, otherwise False
-        """
-        if len(matrix) == 0:
-            return False
-
-        if len(matrix[0]) == 0:
-            return False
-
-        m = len(matrix)
-        n = len(matrix[0])
-
-        low = 0
-        high = (m * n) - 1
-
-        while low <= high:
-            mid = (low + high) // 2
-            if matrix[mid // n][mid % n] == target:
-                return True
-            elif matrix[mid // n][mid % n] > target:
-                high = mid - 1
-            else:
-                low = mid + 1
-
-        return False
-
-    def searchMatrix(self, matrix, target):
-        """
-        Write an efficient algorithm that searches for a value in an m x n matrix. This matrix has
-        the following properties:
-
-        Integers in each row are sorted from left to right.
-        The first integer of each row is greater than the last integer of the previous row.
-        Example 1:
-
-        Input:
-        matrix = [
-          [1,   3,  5,  7],
-          [10, 11, 16, 20],
-          [23, 30, 34, 50]
-        ]
-        target = 3
-        Output: true
-        Example 2:
-
-        Input:
-        matrix = [
-          [1,   3,  5,  7],
-          [10, 11, 16, 20],
-          [23, 30, 34, 50]
-        ]
-        target = 13
-        Output: false
-        Main function for the question
-        Args:
-            matrix: 2D matrix to look a target from
-            target: an integer value to look for
-
-        Returns:
-            bool: True if target exists in the matrix, otherwise False
-        """
-        return self.optimal_approach(matrix, target)
