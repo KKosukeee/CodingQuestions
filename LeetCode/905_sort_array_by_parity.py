4edc978cae5c067cfc6c281ba080295aed17c5d6
kosuke.kuzuoka <kousuke.newlife@gmail.com>
2019-05-26 18:02:47 +0900

    Solved for 905. Sort Array By Parity

--- LeetCode/905_sort_array_by_parity.py
+++ LeetCode/905_sort_array_by_parity.py
@@ -8,6 +8,48 @@ class Solution:
     Runtime: 68 ms, faster than 88.16% of Python3 online submissions for Sort Array By Parity.
     Memory Usage: 13.9 MB, less than 15.18% of Python3 online submissions for Sort Array By Parity.
     """
+    def two_pointer(self, A):
+        """
+        Solves the problem with two pointer approach. At the worst time, the runtime would be O(2N)
+        Args:
+            A: list of integers where we sort by the parity
+
+        Returns:
+            list<int>: where A is sorted in parity
+        """
+        i, j = 0, 0
+        while j < len(A) and i < len(A):
+            if A[j] % 2 == 0:
+                while i < len(A) and i < j and A[i] % 2 == 0:
+                    i += 1
+
+                A[i], A[j] = A[j], A[i]
+
+            j += 1
+        return A
+
+    def quick_sort(self, A):
+        """
+        Does in the similar way as the quick sort does. In the worst, it will be O(N)
+        Args:
+            A: list of integers where we sort by the parity
+
+        Returns:
+            list<int>: where A is sorted in parity
+        """
+        i, j = 0, len(A) - 1
+        while i < j:
+            if A[i] % 2 != 0 and A[j] % 2 == 0:
+                A[i], A[j] = A[j], A[i]
+
+            if A[i] % 2 == 0:
+                i += 1
+
+            if A[j] % 2 != 0:
+                j -= 1
+
+        return A
+
     def sortArrayByParity(self, A):
         """
         Given an array A of non-negative integers, return an array consisting of all the even
@@ -28,13 +70,4 @@ class Solution:
         Returns:
             list<int>: where A is sorted in parity
         """
-        i, j = 0, 0
-        while j < len(A) and i < len(A):
-            if A[j] % 2 == 0:
-                while i < len(A) and i < j and A[i] % 2 == 0:
-                    i += 1
-
-                A[i], A[j] = A[j], A[i]
-
-            j += 1
-        return A
+        return self.quick_sort(A)
