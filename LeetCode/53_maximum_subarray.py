80c17f955ae22a81b2f83ed103c3d46b793c1035
kosuke.kuzuoka <kousuke.newlife@gmail.com>
2019-09-12 09:13:04 +0900

    feat: Solved 1 question for 11th September

--- LeetCode/53_maximum_subarray.py
+++ LeetCode/53_maximum_subarray.py
@@ -2,13 +2,56 @@
 Solution for 53. Maximum Subarray
 https://leetcode.com/problems/maximum-subarray/
 """
+from typing import List
 
 class Solution:
     """
     Runtime: 44 ms, faster than 93.31% of Python3 online submissions for Maximum Subarray.
     Memory Usage: 13.7 MB, less than 5.50% of Python3 online submissions for Maximum Subarray.
     """
-    def maxSubArray(self, nums):
+
+    def top_down(self, nums: List[int]) -> int:
+        """
+        Top-down DP solution which runs in O(N)
+
+        Args:
+            nums(list[int]):
+
+        Returns:
+            int:
+
+        """
+        self.max_sum = float('-inf')
+        def recursion(i):
+            if not i < len(nums):
+                return 0
+            local_max = max(recursion(i + 1) + nums[i], nums[i])
+            self.max_sum = max(self.max_sum, local_max)
+            return local_max
+
+        recursion(0)
+        return self.max_sum
+
+    def bottom_up(self, nums: List[int]) -> int:
+        """
+        Bottom-up DP solution which runs in O(N)
+
+        Args:
+            nums: list of integers to look for maximum subarray
+
+        Returns:
+            list<int>: list of integers which's sum is the maximum within the nums
+        """
+        local_max = nums[0]
+        global_max = nums[0]
+
+        for num in nums[1:]:
+            local_max = max(num, num + local_max)
+            global_max = max(global_max, local_max)
+
+        return global_max
+
+    def maxSubArray(self, nums: List[int]) -> int:
         """
         Given an integer array nums, find the contiguous subarray (containing at least one number)
         which has the largest sum and return its sum.
@@ -24,11 +67,4 @@ class Solution:
         Returns:
             list<int>: list of integers which's sum is the maximum within the nums
         """
-        local_max = nums[0]
-        global_max = nums[0]
-
-        for num in nums[1:]:
-            local_max = max(num, num + local_max)
-            global_max = max(global_max, local_max)
-
-        return global_max
+        return self.bottom_up(nums)
